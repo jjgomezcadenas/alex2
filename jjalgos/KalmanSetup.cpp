7c3d8a00c89b7e5018f63afddfcb82c6cc345335
jjgomezcadenas <jjgomezcadenas@users.noreply.github.com>
2014-10-18 21:02:18 +0200

    adding stuff to KalmenSetup

--- jjalgos/KalmanSetup.cpp
+++ jjalgos/KalmanSetup.cpp
@@ -1,5 +1,6 @@
 #include "KalmanSetup.hh"
 #include <alex/KFSetup.h>
+#include <alex/KFSvc.h>
 #include <alex/LogUtil.h>
 namespace alex {
 
@@ -16,6 +17,8 @@ namespace alex {
                                      matchingVerbosity);
 
     KFSetup::Instance().Print();
+
+    KFSvc::Instance().Init();  // this must be called after KFSetup
   	return true;
   }
 //--------------------------------------------------------------------
@@ -23,6 +26,43 @@ namespace alex {
 //--------------------------------------------------------------------
   {
   	
+    // you must call here the methods to get a trajectory and fit it:
+    /*
+    Trajectory* trj = CreateTrajectory(std::vector<const Hit* > hits, 
+                                      std::vector<double> hitErrors) ;
+
+    State* seed =SeedState(std::vector<double> v0, std::vector<double> p0) ;
+
+    bool status = FitTrajectory(*traj, *seed);
+
+    // Here we must access the Trajectory information and retrieve stuff. 
+
+    This is Sasha's e-mail, don't have much more info. 
+
+    Regarding which info can  be retrieved from nodes as a result of the
+kalman filtering:
+different state hypervector values and residuals are stored with the
+corresponding names:
+E.g. to retrieve predicted dynamic vector and matrix from prediction
+step:
+// get the state
+State& state = node.state();
+const HyperVector& hv_P = state.hv(RP::predicted);
+const EVector& x_P = hv_P.vector();
+const EMatrix& C_P = hv_P.matrix();
+// Retrieve predicted residual
+const HyperVector resHV_P = node.residuals().hv(predicted_name); const
+EVector& r_P = resHV_P.vector();
+const EMatrix& R_P = resHV_P.matrix();
+node.quality(predicted_name,chi2); //or resHV_P.chi2()
+available names:
+RP:predicted, RP::filtered
+RP::smoothed (default)
+
+
+    delete trj;
+    delete seed;
+    */
 
 		return true;
   }
